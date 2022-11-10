bfc12d9ee1f999a14d5c2de94df1a3402e183a45
Joel De Conto <joel_d.c@hotmail.com>
2022-11-10 17:57:52 -0300

    _document.js added

--- pages/contato.js
+++ pages/contato.js
@@ -10,7 +10,7 @@ function Home(){
                 <meta name="description" content="Site em desenvolvimento em Next.js"/>
                 <meta name="author" content="Joel De Conto"/>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+                
                 <title>Testes com Next.js: Contato</title>
             </Head>
 
