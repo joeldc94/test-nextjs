a5d42a5ea92a4115de75c9aef97973856f0df0a3
Joel De Conto <joel_d.c@hotmail.com>
2022-11-22 17:42:50 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
       //SERVER_URL: 'http://localhost:8080/',
-      SERVER_URL: 'https://test-nodejs-qxdglzyno-joeldc94.vercel.app/'
+      SERVER_URL: 'https://test-nodejs-lyart.vercel.app/'
     },
   }
